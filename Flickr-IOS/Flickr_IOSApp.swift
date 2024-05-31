c09b539812f8cd238ede56678beb51a12bd432bb
FulvioResendes1982 <fulvio12@icloud.com>
2024-05-31 15:05:37 -0300

    Initial Commit

---
+++ Flickr-IOS/Flickr_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flickr_IOSApp.swift
+//  Flickr-IOS
+//
+//  Created by fgrmac on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Flickr_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Flickr-IOSUITests/Flickr_IOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flickr_IOSUITestsLaunchTests.swift
+//  Flickr-IOSUITests
+//
+//  Created by fgrmac on 31/05/24.
+//
+
+import XCTest
+
+final class Flickr_IOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
